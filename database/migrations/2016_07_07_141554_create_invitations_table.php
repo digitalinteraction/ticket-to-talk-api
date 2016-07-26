ece627960cf51e3c32c388c9615ffd217012a148
Daniel Welsh <danjwelsh24@gmail.com>
2016-07-26 11:03:03 +0100

    migration fix

--- database/migrations/2016_07_07_141554_create_invitations_table.php
+++ database/migrations/2016_07_07_141554_create_invitations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePersonUserPivotTable extends Migration
+class CreateInvitationsTable extends Migration
 {
     /**
      * Run the migrations.
