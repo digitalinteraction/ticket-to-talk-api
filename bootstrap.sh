51d53edf43e5ac5f3e4e9d56116e80f4fbf91499
Daniel Welsh <danjwelsh24@gmail.com>
2017-03-28 12:45:34 +0100

    DW: + Updated dockerfile to use Rob’s composer image

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "HELLO WORLD"
+
+# php artisan optimize
+
+#//            "Illuminate\\Foundation\\ComposerScripts::postUpdate",
+
+
+# "Illuminate\\Foundation\\ComposerScripts::postInstall",
+#            "php artisan optimize"
+
+
+##!/bin/bash
+#
+### copy env vars into www pool
+#
+## For running the db in the container, ran externally, set in .env
+#php artisan migrate:install
+#php artisan migrate
+#php artisan db:seed --class=InspirationTableSeeder
