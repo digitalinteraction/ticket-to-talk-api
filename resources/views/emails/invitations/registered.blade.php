3ac4d34e47d191f2aa8f9af929eb1f95e7791b97
Daniel Welsh <danjwelsh24@gmail.com>
2017-04-05 13:20:01 +0100

    DW: + Send emails on invite and allow invite recipients to view people

---
+++ resources/views/emails/invitations/registered.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Invitation</title>
+</head>
+<body>
+    <p>
+        Dear {{$name}},
+        <br/>
+        <br/>
+        <strong>{{$sender}}</strong> has invited you to help them create tickets for <strong>{{$person}}</strong>!
+        <br/>
+        If you would like to help <strong>{{$sender}}</strong> create tickets for <strong>{{$person}}</strong> then please:
+        <ol>
+            <li>login to Ticket to Talk</li>
+            <li>Select 'Add a Person'</li>
+            <li>Select 'View Invitations'</li>
+        </ol>
+    </p>
+    <p>
+        Thanks,
+        <br/>
+        <br/>
+        The Ticket to Talk team.
+    </p>
+</body>
+</html>
---
+++ resources/views/emails/invitations/standard.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Invitation</title>
+</head>
+<body>
+    <h3>
+        You have been invited to join Ticket to Talk.
+    </h3>
+    <a href="https://tickettotalk.openlab.ncl.ac.uk"><h4>Find out more!</h4></a>
+
+    <p>
+        Ticket to Talk is a mobile application used to help bridge gaps in conversation between younger and older
+        family members, through the use of photos, sounds, and videos, known as tickets.
+    </p>
+
+    <p>
+        <strong>{{$sender}}</strong> has invited you to help them create tickets for <strong>{{$person}}</strong>!
+        If you would like to help <strong>{{$sender}}</strong> create tickets then please register for Ticket to Talk and accept the invitation.
+    </p>
+    <p>
+        Thanks,
+        <br/>
+        <br/>
+        The Ticket to Talk team.
+    </p>
+</body>
+</html>
