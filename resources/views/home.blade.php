f661fb943ef4dfa9893efde712abbc8b3e1fe969
Daniel Welsh <danjwelsh24@gmail.com>
2017-04-06 09:31:08 +0100

    DW: + Added template for landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Ticket to Talk</title>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Mono" rel="stylesheet">
+    <!--Import materialize.css-->
+    {!! Html::style('css/materialize.css', ['media' => 'screen,projection']) !!}
+    {!! Html::style('css/style.css') !!}
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
-        }
-    </style>
+    <meta title="Ticket to Talk"/>
+    <meta author="Daniel Welsh"/>
+    <meta charset="UTF-8">
+    <meta name="description" content="Ticket to Talk: an application helping to scaffold interactions between younger people and older relatives with dementia.">
 </head>
+
 <body>
-<div class="container">
-    <div class="content">
-        <div class="title">Ticket to Talk API</div>
+
+    <div id="title-block" class="row">
+
+
+        <h1 id="title" class="center-align">
+            ticket-to-talk
+        </h1>
+
+        <div class="container">
+            <div id="title-description" class="card-panel center-align">
+                <p>
+                    An application helping to support conversation between younger people and older relatives with dementia.
+                    <br/>
+                    Use Ticket to Talk to create talking points with older relatives through crafting a collection of pictures, sounds, and YouTube videos.
+                </p>
+            </div>
+        </div>
+
     </div>
-</div>
+
+    <div id="work-flow" class="row">
+
+        <div class="container">
+            <div class="card-panel">
+                
+            </div>
+        </div>
+
+    </div>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+{!! Html::script('js/materialize.js') !!}
+{!! Html::script('js/script.js') !!}
 </body>
 </html>
