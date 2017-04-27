31da9db4c39859b6abac3e9a4e20970c89a323a3
Daniel Welsh <danjwelsh24@gmail.com>
2017-04-27 17:55:59 +0100

    DW: + Added landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
     <link href="https://fonts.googleapis.com/css?family=PT+Mono" rel="stylesheet">
     <!--Import materialize.css-->
     {!! Html::style('css/materialize.css', ['media' => 'screen,projection']) !!}
-    {!! Html::style('css/style.css') !!}
+    {!! Html::style('fonts/font-awesome/css/font-awesome.min.css')!!}
+    {!! Html::style('css/main.css') !!}
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -21,13 +22,16 @@
 
     <div id="title-block" class="row">
 
+        <div class="container">
+    		<div id="title-bar" class="row">
+    			<h1 class="col s12 center-align">ticket to talk</h1>
+    			<h3 class="col s12 center-align">Bridge the Generational Gap</h3>
+    		</div>
+    	</div>
 
-        <h1 id="title" class="center-align">
-            ticket-to-talk
-        </h1>
 
         <div class="container">
-            <div id="title-description" class="card-panel center-align">
+            <div id="title-description" class="card-panel center-align row">
                 <p>
                     An application helping to support conversation between younger people and older relatives with dementia.
                     <br/>
@@ -38,15 +42,83 @@
 
     </div>
 
-    <div id="work-flow" class="row">
-
+    <div id="sign-up" class="row">
         <div class="container">
-            <div class="card-panel">
-                
+            <p>
+                Please enter an email address below if you would like to receive updates on Ticket to Talk or are interested in oppurtunities to take part in future research projects on intergenerational communication within dementia.
+            </p>
+            <div class="col s8 offset-s2 left-align">
+                <div class="input-field col s8">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                </div>
+                <div class="input-field col s4">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">
+                        Join!
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="info" class="row">
+        <div id="about" class="info-box row">
+            <h3 class="info-box-header col l2 offset-l1 s12">About</h3>
+            <div class="col l8 m12 info-box-text">
+                <p>
+                    Ticket to Talk is a smartphone application that aims to help encourage conversation between younger people and grandparents, friends or people they care for who are experiencing dementia. The app is designed to help collect and curate digital media (”tickets”) to be used to prompt and stimulate talk, conversation and reminiscence between younger people and those they are close to with dementia.
+                </p>
+                <p>
+                    Upon opening the app for the first time, you are asked to create a profile about you and a profile about the person with whom you’d like to talk with. You can also add other family members, close friends, or caregivers to the app so they can have access to, or contribute, content to peoples profiles. After this the app guides you to add bits of media - we call these “Tickets”. Tickets are things that can be used as prompts in conversations. Tickets are created in-app by selecting photos from the phone’s library, taking new photos or videos with the camera, or by using the microphone to record songs and sounds.
+                </p>
+                <p>
+                    If you are struggling to come up with ideas for Tickets the app gives some small bits of inspiration to respond to, i.e. “Can you find a picture of Sarah’s old car?”. This helps you to build up a collection quickly. To use the tickets as prompts you are encouraged by the app to curate these into a “Conversation”. This is essentially a playlist of Tickets to be used as a shared resource in conversation. During an in-person conversation with a loved one or the person you care for, you would press play and the tickets would be presented to cycle through (5). At the end of the conversation the app will prompt you to make some short notes on how the conversation went. This can be useful to keep track of conversation topics that they went particularly well, and use these to develop new Tickets for future visits.
+                </p>
+            </div>
+        </div>
+
+        <div id="origin" class="info-box row">
+            <h3 class="info-box-header col l2 offset-l1 s12">Origin</h3>
+            <div class="col l8 m12 info-box-text">
+                <p>
+                    The app has been developed as part of the DemYouth project, where we have been working with groups of young people with personal experiences of dementia. The DemYouth project has been supported through grants from the ESRC, EPSRC and Newcastle University’s Institute for Social Renewal. It is an ongoing collaboration between researchers at Newcastle and Northumbria Universities and partners at the Alzheimer’s Society and Youth Focus North East as part of the larger DemTalk project (www.demtalk.org.uk). The Ticket-to-Talk project has been contributed to by: Tony Young (Newcastle University), Daniel Welsh (Newcastle University), John Vines (Northumbria University), Roisin McNaney (Lancaster University), Kellie Morrissey (Newcastle University), Tom Schofield (Newcastle University), Leon Mexter (Youth Focus North East) and Jamie Mercer (Youth Focus North East).
+                </p>
+            </div>
+        </div>
+
+        <div id="when" class="info-box row">
+            <h3 class="info-box-header col l2 offset-l1 s12">When</h3>
+            <div class="col l8 m12 info-box-text">
+                <div id="when-text" class="center-align">
+                    <span id="available">spring 2017</span>
+                    <div id="store-icons">
+                        <i class="fa fa-apple" aria-hidden="true"></i>
+                        <i class="fa fa-android" aria-hidden="true"></i>
+                    </div>
+                </div>
             </div>
         </div>
 
     </div>
+
+    <footer class="page-footer">
+        <div class="container valign-wrapper">
+            <div id="logos" class="row valign">
+                {{ HTML::image('images/newcastleUni_white.png', 'Newcastle University', array('class' => 'col s2')) }}
+                {{ HTML::image('images/ESRC_white.png', 'Newcastle University', array('class' => 'col s2')) }}
+                {{ HTML::image('images/EPSRC_white.png', 'Newcastle University', array('class' => 'col s2')) }}
+                {{ HTML::image('images/NewcastleUniNISR_white.png', 'Newcastle University', array('class' => 'col s2')) }}
+                {{ HTML::image('images/DigitalEconomy_white.png', 'Newcastle University', array('class' => 'col s2')) }}
+                {{ HTML::image('images/YFNE_white.png', 'Newcastle University', array('class' => 'col s2')) }}
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+            For queries or more information email <a href="mailto:d.welsh@ncl.ac.uk">Daniel Welsh</a>
+            </div>
+          </div>
+    </footer>
 
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
